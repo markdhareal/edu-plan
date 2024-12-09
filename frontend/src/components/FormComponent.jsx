9b3721da9e3429bfc10f8b5d404f43fc978f911c
Dharyl <programmingdha@gmail.com>
2024-12-09 11:17:46 +0800

    Done Form

--- frontend/src/components/FormComponent.jsx
+++ frontend/src/components/FormComponent.jsx
@@ -2,41 +2,70 @@ import React from "react";
 const FormComponent = () => {
   return (
     <>
-      <h1 className="head_text bg-[#9667e0]">Form Component</h1>
-      <form className="space-y-7" action="/generate" method="POST">
-        <input
-          className="px-4 py-2 border text-[#9667e0] rounded-md w-full"
-          type="text"
-          placeholder="Subject"
-        />
-        <input
-          className="px-4 py-2 border text-[#9667e0] rounded-md w-full"
-          type="text"
-          placeholder="Lesson"
-        />
-        <div className="flex space-x-4">
+      <h1 className="head_text text-black">
+        Generate <span className="text-[#9667e0]">Lesson Plan</span>
+      </h1>
+
+      <h3 className="head_text text-black">
+        <span className="text-[#9667e0]">Plan</span> in Seconds
+      </h3>
+
+      <div className="flex items-center justify-center mt-14 max-w-md mx-auto bg-white p-4 rounded-lg shadow-lg">
+        <form action="/generate" method="POST">
+          <p className="text-black mt-4">Subject</p>
           <input
-            className="px-4 py-2 border text-[#9667e0] rounded-md w-1/2"
+            className="px-4 py-2 mb-7 border text-black rounded-md w-full"
             type="text"
-            placeholder="Duration"
+            placeholder="Science"
           />
+
+          <p className="text-black">Lesson</p>
           <input
-            className="px-4 py-2 border text-[#9667e0] rounded-md w-1/2"
+            className="px-4 py-2 mb-7 border text-black rounded-md w-full"
             type="text"
-            placeholder="Grade Level"
+            placeholder="Solar System"
           />
-        </div>
-        <select className="px-4 py-2 border rounded-md w-full" name="gradeType">
-          <option value="elementary">Elementary</option>
-          <option value="highschool">High School</option>
-        </select>
-        <button
-          className="font-bold px-6 py-4 text-white rounded w-full bg-[#9667e0] hover:bg-[#d4bbfc] hover:text-[#2C2C2C]"
-          type="submit"
-        >
-          Generate
-        </button>
-      </form>
+
+          <div className="flex space-x-4 w-full mb-7">
+            <div className="w-1/2">
+              <p className="text-black">Duration</p>
+              <input
+                className="px-4 py-2 border text-black rounded-md w-full"
+                type="text"
+                placeholder="1 hour"
+              />
+            </div>
+
+            <div className="w-1/2">
+              <p className="text-black">Grade Level</p>
+              <input
+                className="px-4 py-2 border text-black rounded-md w-full"
+                type="text"
+                placeholder="Grade 4 or 4th Grade"
+              />
+            </div>
+          </div>
+
+          <p className="text-black">Type</p>
+          <select
+            className="px-4 py-2 text-black border rounded-md w-full mb-7"
+            name="gradeType"
+          >
+            <option className="text-black" value="elementary">
+              Elementary
+            </option>
+            <option className="text-black" value="highschool">
+              High School
+            </option>
+          </select>
+          <button
+            className="font-bold px-6 py-4 text-white rounded w-full bg-[#9667e0] transition-all duration-200 shadow-md hover:ring-[#b590f1] hover:ring-4 active:scale-95 active:bg-[#b590f1]"
+            type="submit"
+          >
+            Generate
+          </button>
+        </form>
+      </div>
     </>
   );
 };
--- frontend/src/components/OutputComponents.jsx
+++ frontend/src/components/OutputComponents.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const OutputComponents = () => {
   return (
     <>
-      <h1 className="head_text bg-[#9667e0]">Output Component</h1>
+      <h1 className="head_text text-black">
+        Get the <span className="text-[#9667e0]">Output</span> here
+      </h1>
     </>
   );
 };
