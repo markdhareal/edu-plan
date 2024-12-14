3b0ab0ad220a44c9a9321483ffc99f5d523fba8e
Dharyl <programmingdha@gmail.com>
2024-12-14 14:19:46 +0800

    Removed copy to clipboar feature

--- frontend/src/components/OutputComponents.jsx
+++ frontend/src/components/OutputComponents.jsx
@@ -1,25 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactMarkdown from "react-markdown";
 
 const OutputComponents = ({ lessonPlan }) => {
   console.log("Lesson Plan in OutputComponents:", lessonPlan);
-
-  const [copied, setCopied] = useState(false);
-
-  const handleCopy = () => {
-    navigator.clipboard
-      .writeText(lessonPlan)
-      .then(() => {
-        setCopied(true);
-
-        setTimeout(() => {
-          setCopied(false);
-        }, 2000);
-      })
-      .catch((error) => {
-        console.log("Error: ", error);
-      });
-  };
 
   return (
     <>
@@ -27,7 +10,7 @@ const OutputComponents = ({ lessonPlan }) => {
         Get the <span className="text-[#9667e0]">Output</span> here
       </h1>
       <div className="bg-[#9667e0] w-full h-1 mt-6" />
-      <div className="m-5 h-[72vh]  overflow-y-auto">
+      <div className="m-5 h-[75vh]  overflow-y-auto">
         {lessonPlan ? (
           <ReactMarkdown>{lessonPlan}</ReactMarkdown>
         ) : (
@@ -35,14 +18,6 @@ const OutputComponents = ({ lessonPlan }) => {
             **Your Generated Lesson Plan will appear here.**
           </ReactMarkdown>
         )}
-      </div>
-      <div className="flex flex-col">
-        <button
-          onClick={handleCopy}
-          className="font-bold px-6 py-4 mx-auto text-white rounded-full max-w-[250px] bg-[#9667e0] transition-all duration-200 shadow-md hover:ring-[#b590f1] hover:ring-4 active:scale-95 active:bg-[#b590f1]"
-        >
-          {copied ? "Copied!" : "Copy to Clipboard"}
-        </button>
       </div>
     </>
   );
